719b0f1517cde269003c60d52161daaf07080171
idea-ai-labs <idea.ai.labs@gmail.com>
2025-11-13 23:46:40 -0500

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,6 +1,12 @@
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 export default function handler(req, res) {
-  res.setHeader('Content-Type', 'application/xml');
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  res.setHeader("Content-Type", "application/xml");
+  res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://resume-samurai.vercel.app/</loc>
@@ -10,6 +16,5 @@ export default function handler(req, res) {
     <loc>https://resume-samurai.vercel.app/app</loc>
     <priority>0.8</priority>
   </url>
-</urlset>`;
-  res.status(200).send(sitemap);
+</urlset>`);
 }
