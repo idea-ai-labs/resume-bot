32f23feb616d90d23698cc194ec23fb490d97646
idea-ai-labs <idea.ai.labs@gmail.com>
2025-11-13 23:57:22 -0500

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -6,6 +6,7 @@ export const config = {
 
 export default function handler(req, res) {
   res.setHeader("Content-Type", "application/xml");
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
   res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
