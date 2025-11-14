3909c8647de5d58d2d1c3492f25a7d0893a41251
idea-ai-labs <idea.ai.labs@gmail.com>
2025-11-14 06:53:08 -0500

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -6,16 +6,38 @@ export const config = {
 
 export default function handler(req, res) {
   res.setHeader("Content-Type", "application/xml");
-  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
-  res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
+  res.setHeader(
+    "Cache-Control",
+    "no-store, no-cache, must-revalidate, proxy-revalidate"
+  );
+
+  const pages = [
+    "/", // index.html
+    "/about.html",
+    "/terms.html",
+    "/contact.html",
+    "/privacy.html",
+    "/testimonials.html",
+    "/how-we-are-different.html",
+    "/blog/post1.html",
+    "/blog/post2.html",
+    "/blog/post3.html"
+  ];
+
+  const urls = pages
+    .map(
+      (path) => `
+<url>
+  <loc>https://resume-samurai.vercel.app${path}</loc>
+  <priority>0.8</priority>
+</url>`
+    )
+    .join("");
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>https://resume-samurai.vercel.app/</loc>
-    <priority>1.0</priority>
-  </url>
-  <url>
-    <loc>https://resume-samurai.vercel.app/app</loc>
-    <priority>0.8</priority>
-  </url>
-</urlset>`);
+${urls}
+</urlset>`;
+
+  res.status(200).send(xml);
 }
